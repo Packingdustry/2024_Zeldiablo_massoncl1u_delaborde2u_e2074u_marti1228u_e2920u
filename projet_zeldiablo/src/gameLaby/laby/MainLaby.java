68f6cca649a6bbeb90dc458bf46f4e991d8d3c14
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-05-27 15:33:14 +0200

    Main Laby

---
+++ projet_zeldiablo/src/gameLaby/laby/MainLaby.java
@@ -0,0 +1,24 @@
+package gameLaby.laby;
+
+import moteurJeu.MoteurJeu;
+import java.io.IOException;
+
+public class MainLaby {
+
+    public static void main(String[] args) throws IOException {
+        int width = 800;
+        int height = 600;
+        int pFPS = 100;
+
+        // creation des objets
+        LabyJeu jeu = new LabyJeu();
+        LabyDessin dessin = new LabyDessin();
+
+        // parametrage du moteur de jeu
+        MoteurJeu.setTaille(width,height);
+        MoteurJeu.setFPS(pFPS);
+
+        // lancement du jeu
+        MoteurJeu.launch(jeu, dessin);
+    }
+}
