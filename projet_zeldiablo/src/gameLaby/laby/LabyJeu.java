64168abc63ceb2f27486924576e0db3d54d05c03
Packingdustry <alois.mc@hotmail.com>
2024-05-27 14:56:07 +0200

    ajoute LabyJeu complet

--- projet_zeldiablo/src/gameLaby/laby/LabyJeu.java
+++ projet_zeldiablo/src/gameLaby/laby/LabyJeu.java
@@ -3,10 +3,35 @@ package gameLaby.laby;
 import moteurJeu.Clavier;
 import moteurJeu.Jeu;
 
+import javax.swing.plaf.LabelUI;
+import java.io.IOException;
+
 public class LabyJeu implements Jeu {
+    public static final String FICHIER = "labySimple/laby1.txt";
+
+    private final Labyrinthe labyrinthe;
+
+    public LabyJeu() throws IOException {
+        labyrinthe = new Labyrinthe(FICHIER);
+    }
+
     @Override
     public void update(double secondes, Clavier clavier) {
+        if (clavier.droite) {
+            labyrinthe.deplacerPerso(Labyrinthe.DROITE);
+        }
 
+        if (clavier.gauche) {
+            labyrinthe.deplacerPerso(Labyrinthe.GAUCHE);
+        }
+
+        if (clavier.bas) {
+            labyrinthe.deplacerPerso(Labyrinthe.BAS);
+        }
+
+        if (clavier.haut) {
+            labyrinthe.deplacerPerso(Labyrinthe.HAUT);
+        }
     }
 
     @Override
@@ -16,6 +41,6 @@ public class LabyJeu implements Jeu {
 
     @Override
     public boolean etreFini() {
-
+        return false;
     }
 }
