7600a80d0f320eec5727e6ce8106db0e10ee77fb
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-06-05 14:13:15 +0200

    ajout classe test

---
+++ projet_zeldiablo/test/gameLaby/laby/LabyrintheTest.java
@@ -0,0 +1,24 @@
+package gameLaby.laby;
+
+import moteurJeu.MoteurJeu;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabyrintheTest {
+
+    @Test
+    void testVerifierMonstre() throws IOException {
+
+    }
+
+    @Test
+    void testGetPerso() {
+    }
+
+    @Test
+    void testGetMonstre() {
+    }
+}
