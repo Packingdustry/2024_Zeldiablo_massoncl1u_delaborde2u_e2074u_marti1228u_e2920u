e00607b20f2f0a382b36d7a70a07bdc5071be05a
Martin <marti1228u@etu.univ-lorraine.fr>
2024-06-05 10:58:07 +0200

    ajout bombe v1

---
+++ projet_zeldiablo/src/gameLaby/laby/Bombe.java
@@ -0,0 +1,30 @@
+package gameLaby.laby;
+
+public class Bombe extends Perso{
+    /**
+     * constructeur
+     *
+     * @param dx position selon x
+     * @param dy position selon y
+     * @param dgt degat de la bombe
+     */
+    public Bombe(int dx, int dy, int dgt){
+        super(dx,dy,dgt);
+    }
+
+    public void mettredgt(Perso p){
+        int dgt=this.getVie();
+        p.vie-=dgt;
+    }
+
+    public void explose(){
+        int x = this.getX();
+        int y = this.getY();
+
+        for(int i=x;i<=3;i++){
+            for(int j=y;j<=3;j++){
+                this.
+            }
+        }
+    }
+}
