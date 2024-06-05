707335fe10eebd93336e70ba9fedb3628baa3d07
Quent1540 <dieudonnequent1@gmail.com>
2024-06-05 15:30:45 +0200

    tests

--- projet_zeldiablo/test/gameLaby/laby/LabyrintheTest.java
+++ projet_zeldiablo/test/gameLaby/laby/LabyrintheTest.java
@@ -1,4 +1,6 @@
 package gameLaby.laby;
+import gameLaby.laby.Labyrinthe;
+import gameLaby.laby.Perso;
 
 import moteurJeu.MoteurJeu;
 import org.junit.jupiter.api.Test;
@@ -8,17 +10,21 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LabyrintheTest {
-
     @Test
-    void testVerifierMonstre() throws IOException {
-
+    void testGetPerso() throws IOException {
+        Labyrinthe l = new Labyrinthe("laby0.txt");
+        Perso principal = new Perso(2,3,50);
+        l.persos.add(principal);
+        assertEquals(null,l.getPerso(3,4),"La methode doit renvoyer null car le perso nest pas en 3,4");
+        assertEquals(principal,l.getPerso(2,3),"La methode doit renvoyer le personnage principal car il est bien en 2,3");
     }
 
     @Test
-    void testGetPerso() {
-    }
-
-    @Test
-    void testGetMonstre() {
+    void testGetMonstre() throws IOException {
+        Labyrinthe l = new Labyrinthe("laby0.txt");
+        Monstre m = new Monstre(5,50,3,3);
+        l.persos.add(m);
+        assertEquals(null,l.getMonstre(3,4),"La methode doit renvoyer null car le monstre nest pas en 3,4");
+        assertEquals(m,l.getPerso(3,3),"La methode doit renvoyer le monstre car il est bien en 3,3");
     }
 }
