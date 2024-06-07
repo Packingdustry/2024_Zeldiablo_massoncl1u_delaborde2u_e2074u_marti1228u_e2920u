cc13391ced3e6b1b9c7ea96157db3c01f053b2ca
Baptiste Delaborde <baptiste.delaborde8@etu.univ-lorraine.fr>
2024-06-07 15:22:05 +0200

    ajout des classes Item et Bouclier

---
+++ projet_zeldiablo/src/gameLaby/laby/Bouclier.java
@@ -0,0 +1,9 @@
+package gameLaby.laby;
+
+public class Bouclier extends Item {
+    int vie;
+    public Bouclier(int abscisse, int ordonne, int v) {
+        super(abscisse, ordonne);
+        this. vie = v;
+    }
+}
